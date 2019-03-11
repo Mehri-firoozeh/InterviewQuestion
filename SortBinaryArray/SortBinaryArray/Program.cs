6f33bf0d785ec7215c7479869ba921e2a18122ae
mehrikal <mehrikal@seattleu.edu>
2019-03-11 12:51:05 -0700

    Initialize new question

---
+++ SortBinaryArray/SortBinaryArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortBinaryArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
